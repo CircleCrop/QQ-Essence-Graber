57de34ae91cadc73876922a10bded77d0aa54b31
CircleCrop <54088075+CircleCrop@users.noreply.github.com>
2023-01-25 22:06:05 +0800

    Add files via upload
    Main

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys, os, subprocess
+order = 1
+cmd = 'python essence.py '
+while order <= 1001:
+    print('####')
+    cout = cmd + str(order)
+    order = order + 1
+    os.system(cout)
+print('####')

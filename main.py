5b2363a219e3b1f5282982e501a0337ae36e634c
CircleCrop <54088075+CircleCrop@users.noreply.github.com>
2023-01-26 01:04:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import essence
 order = 1
-group_id = 11111
+group_id = 1234567
 cookie = ''
-
+while(order<=1000):
+    out = str(essence.get(order,group_id,cookie)) + ','
+    print(out)
+    order += 1
